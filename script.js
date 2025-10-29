7d6416ac09869a72e8043183ebdb0c8791136c4c
Ishank Anand <ishank.prinshu@gmail.com>
2025-10-30 04:35:41 +0530

    implemented main feature

--- script.js
+++ script.js
@@ -1,32 +1,83 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Image width slider
-    const leftSliderEl = document.querySelector(".slider.left")
-    const rightSliderEl = document.querySelector(".slider.right")
-    const percentEl = document.querySelector("#percent")
-    function leftSliderScript() {
-        rightSliderEl.value = leftSliderEl.value;
-        processProgressRange();
+    const sizeSliderDivEl = document.querySelector(".sizeSlider");
+    const leftSliderEl = document.querySelector(".slider.left");
+    const rightSliderEl = document.querySelector(".slider.right");
+    const percentEl = document.querySelector("#percent");
+    const startButton = document.querySelector("#startButton");
+    const imageUploader = document.getElementById("imageUpload");
+    const hiddenCanvas = document.getElementById("hiddenCanvas");
+    const hiddenCtx = hiddenCanvas.getContext('2d')
+    const mainCanvas = document.getElementById("mainCanvas");
+    const mainCtx = mainCanvas.getContext('2d');
+    let sliderColour = '#ffae00ff';
+    let worker = new Worker('worker.js');
+    let imageData = null;
+    worker.onmessage = (event) => {
+        processData(event.data)
     }
-    function rightSliderScript() {
-        leftSliderEl.value = rightSliderEl.value;
-        processProgressRange();
+    function processData(outputData) {
+        if (outputData.type === 'progress') {
+            processSliderValue(outputData.data, 'progress');
+        }
+        else if (outputData.type === 'result') {
+            removalWidth = outputData.data;
+            console.log('result', removalWidth);
+            leftSliderEl.disabled = false;
+            rightSliderEl.disabled = false;
+        }
+        else if (outputData.type === 'rebuiltImage') {
+            renderImage(outputData.data);
+        }
     }
-    function processProgressRange() {
-        const sliderValue = rightSliderEl.value;
-        leftSliderEl.style.background = `linear-gradient(to left, #007bff ${sliderValue}%, #ccc ${sliderValue}%)`;
-        rightSliderEl.style.background = `linear-gradient(to right, #007bff ${sliderValue}%, #ccc ${sliderValue}%)`;
-        percentEl.textContent = `${sliderValue}%`;
+    function setSliderDesignToLoader()
+    {
+        sliderColour = '#ffae00ff';
+        leftSliderEl.style.height = `${10}px`;
+        rightSliderEl.style.height = `${10}px`;
     }
-    leftSliderEl.addEventListener('input', leftSliderScript);
-    rightSliderEl.addEventListener('input', rightSliderScript);
-    
-    processProgressRange();
-
+    let canvasStyleScaleFactor = 1;
+    function renderImage(imageData) {
+        mainCanvas.width = imageData.width;
+        mainCanvas.style.width = `${canvasStyleScaleFactor*imageData.width}px`;
+        mainCtx.putImageData(imageData, 0, 0);
+    }
+    function processSliderValue(sliderValue, valueType) {
+        leftSliderEl.value = sliderValue;
+        rightSliderEl.value = sliderValue;
+        percentEl.textContent = `${Math.floor(sliderValue)}%`;
+        leftSliderEl.style.background = `linear-gradient(to left, ${sliderColour} ${sliderValue}%, #ccc ${sliderValue}%)`;
+        rightSliderEl.style.background = `linear-gradient(to right, ${sliderColour} ${sliderValue}%, #ccc ${sliderValue}%)`;
+        if (valueType === 'width') {
+            worker.postMessage({
+                type: 'width',
+                data: Math.floor((sliderValue * imageData.width) / 100)
+            });
+        }
+    }
+    function syncSliders(event) {
+        processSliderValue(event.target.value, 'width');
+    }
+    leftSliderEl.addEventListener('input', syncSliders);
+    rightSliderEl.addEventListener('input', syncSliders);
+    function showSlider() {
+        if (sizeSliderDivEl.classList.contains('hidden')) {
+            sizeSliderDivEl.classList.remove('hidden');
+            percentEl.classList.remove('hidden');
+        }
+    }
+    function hideSlider() {
+        if (!sizeSliderDivEl.classList.contains('hidden')) {
+            sizeSliderDivEl.classList.add('hidden');
+            percentEl.classList.add('hidden');
+        }
+    }
+    hideSlider();
     // Give proper size to the image displayed
     function calcSize(w, h) {
         const minWidth = 700;
         const maxWidth = 800;
-        const maxHeight = window.innerHeight * 0.7;
+        const maxHeight = window.innerHeight * 0.6;
         const aspectRatio = h / w;
         const scale = Math.min(maxWidth / w, maxHeight / h, 1);
         let displayWidth = w * scale;
@@ -35,16 +86,10 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         return [displayWidth, displayWidth * aspectRatio];
     }
-    
-    const imageUploader = document.getElementById("imageUpload");
-    const hiddenCanvas = document.getElementById("hiddenCanvas");
-    const hiddenCtx = hiddenCanvas.getContext('2d')
-    const mainCanvas = document.getElementById("mainCanvas");
-    const mainCtx = mainCanvas.getContext('2d');
-    
+
+
     // Load Image
-    let imageData = null;
-    
+
     function loadImage(file) {
         const reader = new FileReader();
         reader.onload = (fileLoadedEvent) => {
@@ -61,8 +106,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 // sets display size of canvas(scaling)
                 mainCanvas.style.width = `${displayWidth}px`;
                 mainCanvas.style.height = `${displayHeight}px`;
+                canvasStyleScaleFactor = displayWidth / mainCanvas.width;
                 mainCtx.drawImage(hiddenCanvas, 0, 0);
                 imageData = hiddenCtx.getImageData(0, 0, img.width, img.height);
+                leftSliderEl.style.width = `${displayWidth / 2 + 5}px`;
+                rightSliderEl.style.width = `${displayWidth / 2 + 5}px`;
             }
             img.src = fileLoadedEvent.target.result;
         };
@@ -71,25 +119,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     imageUploader.addEventListener('change', (fileUploadEvent) => {
         const file = fileUploadEvent.target.files[0];
-        if(!file) return;
+        if (!file) return;
+        hideSlider();
+        startButton.disabled = false;
         loadImage(file);
     });
 
     // start computation to get outputData
-    const startButton = document.querySelector("#startButton");
-    let worker = new Worker('worker.js');
+    let removalWidth = null;
     function startComputation() {
-        if(!imageData) return;
-        console.log("computation started!!");
-        console.log(imageData);
-        worker.postMessage(imageData);
-        worker.onmessage = (event) => {
-            processData(event.data)
-        }
+        if (!imageData) return;
+        processSliderValue(0);
+        // disable slider and button
+        leftSliderEl.disabled = true;
+        rightSliderEl.disabled = true;
+        startButton.disabled = true;
+        setSliderDesignToLoader()
+        showSlider();
+        worker.postMessage({ type: 'imageData', data: imageData });
     };
-    function processData(outputData) {
-        console.log("processed!!");
-        console.log(outputData);
-    }
     startButton.addEventListener('click', startComputation);
 });
--- worker.js
+++ worker.js
@@ -1,182 +1,198 @@
+let removalHistory = null;
+let imageData = null;
 onmessage = (message) => {
-    console.log("worker got message!");
-    let imageData = message.data;
-    postMessage(calcSeamHistory(imageData));
+    if (message.data.type == 'imageData') {
+        imageData = message.data.data;
+        calcRemovalWidth(imageData)
+        postMessage({ type: 'result', data: removalHistory });
+    }
+    else if (message.data.type == 'width') {
+        console.log("ayoooo");
+        
+        let reqWidth = message.data.data;
+        postMessage({ type: 'rebuiltImage', data: buildImage(reqWidth) });
+    }
 }
 
-function calcSeamHistory(originalImage)
-{
+function calcRemovalWidth(originalImage) {
+    let h = originalImage.height;
+    let w = originalImage.width;
     let croppedData = {
-        height: originalImage.height,
-        width: originalImage.width,
+        height: h,
+        width: w,
         greyScale: generateGreyScale(originalImage),
-        originalColMap: generateColMap(originalImage.height, originalImage.width),
+        originalColMap: generateColMap(h, w),
+        dp: new Float64Array(w * h)
     };
 
     // removalWidth[r][c] = w means tells that to transition from w to w-1, (r, c) has to be removed
-    let removalHistory = {
-        height: originalImage.height,
-        width: originalImage.width,
-        removalWidth: Array(originalImage.height).fill(0).map(x => Array(originalImage.width).fill(0))
+    removalHistory = {
+        height: h,
+        width: w,
+        removalWidth: new Uint32Array(w * h)
     };
-    let seamCols = null;
+    let seamCols = new Uint32Array(h);
     let prevPercentageDone = 0;
     let currPercentageDone = 0;
-    postMessage(currPercentageDone);
-    while(croppedData.width > 0)
-    {
-        seamCols = findSeamCols(croppedData);
+    postMessage({ type: 'progress', data: 0 });
+    while (croppedData.width > 0) {
+        findSeamCols(croppedData, seamCols);
         markSeamWidth(seamCols, removalHistory, croppedData);
         removeSeam(seamCols, croppedData);
         prevPercentageDone = currPercentageDone;
-        currPercentageDone = Math.floor(((originalImage.width - croppedData.width) * 100) / originalImage.width);
-        if(prevPercentageDone != currPercentageDone) postMessage(currPercentageDone);
+        // currPercentageDone = Math.floor(((originalImage.width - croppedData.width) * 100) / originalImage.width);
+        let W = originalImage.width;
+        let w = croppedData.width;
+        currPercentageDone = ((W*W - w*w) * 100) / (W*W);
+        if (prevPercentageDone != currPercentageDone) postMessage({ type: 'progress', data: currPercentageDone });
     }
-    return removalHistory;
 }
-function generateGreyScale(originalImage)
-{
+function buildImage(reqWidth) {
+    let originalWidth = imageData.width;
+    let height = imageData.height;
+    if(reqWidth == 0) return new ImageData(1, height);
+    let removalWidth = removalHistory.removalWidth;
+    let originalImagePixels = imageData.data;
+    let newImagePixels = new Uint8ClampedArray(4 * reqWidth * height);
+    let i = 0;
+    for (let r = 0; r < height; ++r) {
+        for (let c = 0; c < originalWidth; ++c) {
+            if (removalWidth[r*originalWidth + c] > reqWidth) continue;
+            newImagePixels[i] = originalImagePixels[4 * (r * originalWidth + c)];
+            ++i;
+            newImagePixels[i] = originalImagePixels[4 * (r * originalWidth + c) + 1];
+            ++i;
+            newImagePixels[i] = originalImagePixels[4 * (r * originalWidth + c) + 2];
+            ++i;
+            newImagePixels[i] = originalImagePixels[4 * (r * originalWidth + c) + 3];
+            ++i;
+        }
+    }
+    return new ImageData(newImagePixels, reqWidth, height);
+}
+function generateGreyScale(originalImage) {
     let width = originalImage.width;
     let height = originalImage.height;
     let pixelArray = originalImage.data;
-    let greyScale = Array(height).fill(0).map(x => Array(width).fill(0));
-    for(let r = 0; r < height; ++r)
-    {
-        for(let c = 0; c < width; ++c)
-        {
+    let greyScale = new Float64Array(width * height);
+    for (let r = 0; r < height; ++r) {
+        for (let c = 0; c < width; ++c) {
             let R = (r * width + c) * 4;
             let G = R + 1;
             let B = R + 2;
-            greyScale[r][c] = 0.299 * pixelArray[R] + 0.587 * pixelArray[G] + 0.114 * pixelArray[B];
+            greyScale[r * width + c] = 0.299 * pixelArray[R] + 0.587 * pixelArray[G] + 0.114 * pixelArray[B];
         }
     }
     return greyScale;
 }
-function generateColMap(height, width)
-{
-    let posMap = Array(height).fill(0).map(x => Array(width).fill(0));
-    for(let r = 0; r < height; ++r)
-    {
-        for(let c = 0; c < width; ++c)
-        {
-            posMap[r][c] = c;
+function generateColMap(height, width) {
+    let colMap = new Uint32Array(width * height);
+    for (let r = 0; r < height; ++r) {
+        for (let c = 0; c < width; ++c) {
+            colMap[r * width + c] = c;
         }
     }
-    return posMap;
+    return colMap;
 }
 
-function findSeamCols(croppedData)
-{
+function findSeamCols(croppedData, seamCols) {
     let width = croppedData.width;
     let height = croppedData.height;
-    let dp = sobelFilter(croppedData);
-    for(let r = 1; r < height; ++r)
-    {
-        for(let c = 0; c < width; ++c)
-        {
-            let prevMin = dp[r-1][c];
-            if(0 <= c-1 && dp[r-1][c-1] < prevMin)
-            {
-                prevMin = dp[r-1][c-1];
+    sobelFilter(croppedData);
+    let dp = croppedData.dp;
+    for (let r = 1; r < height; ++r) {
+        for (let c = 0; c < width; ++c) {
+            let prevMin = dp[(r - 1) * width + c];
+            if (0 <= c - 1 && dp[(r - 1) * width + (c - 1)] < prevMin) {
+                prevMin = dp[(r - 1) * width + (c - 1)];
             }
-            if(c+1 < width && dp[r-1][c+1] < prevMin)
-            {
-                prevMin = dp[r-1][c+1];
+            if (c + 1 < width && dp[(r - 1) * width + (c + 1)] < prevMin) {
+                prevMin = dp[(r - 1) * width + (c + 1)];
             }
-            dp[r][c] += prevMin;
+            dp[r * width + c] += prevMin;
         }
     }
-    // seamCol[r] stores column to be removed at height r in croppedData
-    let seamCol = Array(height).fill(0);
+    // seamCols[r] stores column to be removed at height r in croppedData
     let minVal = Number.MAX_SAFE_INTEGER;
-    for(let c = 0; c < width; ++c)
-    {
-        if(dp[height-1][c] < minVal)
-        {
-            minVal = dp[height-1][c];
-            seamCol[height-1] = c;
+    for (let c = 0; c < width; ++c) {
+        if (dp[(height - 1) * width + c] < minVal) {
+            minVal = dp[(height - 1) * width + c];
+            seamCols[height - 1] = c;
         }
     }
-    for(let r = height-2; r >= 0; --r)
-    {
+    for (let r = height - 2; r >= 0; --r) {
         minVal = Number.MAX_SAFE_INTEGER;
-        if(seamCol[r+1]-1 >= 0 && dp[r][seamCol[r+1]-1] < minVal)
-        {
-            minVal = Math.min(minVal, dp[r][seamCol[r+1]-1]);
-            seamCol[r] = seamCol[r+1]-1;
+        if (seamCols[r + 1] - 1 >= 0 && dp[r * width + seamCols[r + 1] - 1] < minVal) {
+            minVal = Math.min(minVal, dp[r * width + seamCols[r + 1] - 1]);
+            seamCols[r] = seamCols[r + 1] - 1;
         }
-        if(dp[r][seamCol[r+1]] < minVal)
-        {
-            minVal = Math.min(minVal, dp[r][seamCol[r+1]]);
-            seamCol[r] = seamCol[r+1];
+        if (dp[r * width + seamCols[r + 1]] < minVal) {
+            minVal = Math.min(minVal, dp[r * width + seamCols[r + 1]]);
+            seamCols[r] = seamCols[r + 1];
         }
-        if(seamCol[r+1]+1 < width && dp[r][seamCol[r+1]+1] < minVal)
-        {
-            minVal = Math.min(minVal, dp[r][seamCol[r+1]+1]);
-            seamCol[r] = seamCol[r+1]+1;
+        if (seamCols[r + 1] + 1 < width && dp[r * width + seamCols[r + 1] + 1] < minVal) {
+            minVal = Math.min(minVal, dp[r * width + seamCols[r + 1] + 1]);
+            seamCols[r] = seamCols[r + 1] + 1;
         }
     }
-    return seamCol;
+    return seamCols;
 }
 
 // scharr instead of sobel
 let sobelX = [[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]];
 let sobelY = [[-3, -10, -3], [0, 0, 0], [3, 10, 3]];
-function sobelFilter(croppedData)
-{
-    let imageHeight = croppedData.height;
-    let imageWidth = croppedData.width;
-    let sobelData = Array(imageHeight).fill(0).map(x => Array(imageWidth).fill(0))
-    for(let r = 0; r < imageHeight; ++r)
-    {
-        for(let c = 0; c < imageWidth; ++c)
-        {
+function sobelFilter(croppedData) {
+    let height = croppedData.height;
+    let width = croppedData.width;
+    for (let r = 0; r < height; ++r) {
+        for (let c = 0; c < width; ++c) {
             let gX = 0;
             let gY = 0;
-            for(let dr = -1; dr <= 1; ++dr)
-            {
-                for(let dc = -1; dc <= 1; ++dc)
-                {
+            for (let dr = -1; dr <= 1; ++dr) {
+                for (let dc = -1; dc <= 1; ++dc) {
                     let fr = r + dr;
                     let fc = c + dc;
                     if (fr < 0) fr = 0;
-                    if (fr >= imageHeight) fr = imageHeight - 1;
+                    if (fr >= height) fr = height - 1;
                     if (fc < 0) fc = 0;
-                    if (fc >= imageWidth) fc = imageWidth - 1;
+                    if (fc >= width) fc = width - 1;
 
-                    let I = croppedData.greyScale[fr][fc];
-        
+                    let I = croppedData.greyScale[fr * width + fc];
                     gX += sobelX[1 + dr][1 + dc] * I;
                     gY += sobelY[1 + dr][1 + dc] * I;
                 }
             }
             gX = Math.abs(gX);
             gY = Math.abs(gY);
-            sobelData[r][c] = gX + gY;
-            // sobelData[r][c] = (Math.max(gX, gY) + gX + gY) >> 1;
+            // croppedData.dp[r * width + c] = gX + gY;
+            // croppedData.dp[r * width + c] = (Math.max(gX, gY) + gX + gY) >> 1;;
+            croppedData.dp[r * width + c] = Math.sqrt(gX*gX + gY*gY) ;
         }
     }
-    return sobelData;
 }
 
-function markSeamWidth(seamCols, removalHistory, croppedData)
-{
-    let height = croppedData.height;  
+function markSeamWidth(seamCols, removalHistory, croppedData) {
+    let height = croppedData.height;
     let width = croppedData.width;
-    for(let r = 0; r < height; ++r)
-    {
-        let originalCol = croppedData.originalColMap[r][seamCols[r]];
-        removalHistory.removalWidth[r][originalCol] = width;
+    let originalWidth = removalHistory.width;
+    for (let r = 0; r < height; ++r) {
+        let originalCol = croppedData.originalColMap[r * width + seamCols[r]];
+        removalHistory.removalWidth[r * originalWidth + originalCol] = width;
     }
 }
-function removeSeam(seamCols, croppedData)
-{
-    let height = croppedData.height;  
+function removeSeam(seamCols, croppedData) {
+    let height = croppedData.height;
     let width = croppedData.width;
-    for(let r = 0; r < height; ++r)
-    {
-        croppedData.greyScale[r].splice(seamCols[r], 1);
-        croppedData.originalColMap[r].splice(seamCols[r], 1);
+    let i = 0;
+    for (let r = 0; r < height; ++r) {
+        for (let c = 0; c < width; ++c) {
+            if (c == seamCols[r]) {
+                continue;
+            }
+            croppedData.greyScale[i] = croppedData.greyScale[r * width + c];
+            croppedData.originalColMap[i] = croppedData.originalColMap[r * width + c];
+            ++i;
+        }
     }
     croppedData.width = croppedData.width - 1;
 }
